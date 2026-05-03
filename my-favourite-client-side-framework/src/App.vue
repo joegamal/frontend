23e5b94da7d156b98ddfc7240bfb2c12b249573c
joegamal <yusufmagamalth@gmail.com>
2026-05-03 15:35:24 +0300

    vue start kit

---
+++ my-favourite-client-side-framework/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <h1>You did it!</h1>
+    <p>Finally</p>
+  </div>
+</template>
+
+<style scoped>
+
+.main{
+  margin: 0%;
+  padding: 0%;
+  margin-top: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
